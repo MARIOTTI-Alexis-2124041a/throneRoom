1b1d1e2fe3ae1d74b4300e87e9a51272f7daf66d
alexis <alexis.mariotti@laposte.net>
2023-10-16 11:02:48 +0200

    created globals.cpp

---
+++ src/globals.cpp
@@ -0,0 +1,15 @@
+//
+// Created by alexis on 16/10/23.
+//
+
+#include <semaphore>
+
+using namespace std;
+
+unsigned  n = 10;
+unsigned  subjects = 0;
+binary_semaphore mutex(1);
+binary_semaphore king(0);
+binary_semaphore subject(0);
+binary_semaphore kingDone(0);
+binary_semaphore subjectDone(0);
